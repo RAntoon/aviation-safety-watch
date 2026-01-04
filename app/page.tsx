683d3d36e16b32e1c784f8f757bc8ed40b0b4c95
RAntoon <ryan.antoon@gmail.com>
2026-01-03 23:11:14 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import dynamic from "next/dynamic";
 
-const MapView = dynamic(() => import("./components/MapView"), {
-  ssr: false,
-  loading: () => <div style={{ padding: 20 }}>Loading map…</div>
-});
+const MapView = dynamic(() => import("./components/MapView"), { ssr: false });
 
 export default function Page() {
   return <MapView />;
