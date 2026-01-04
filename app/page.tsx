36c1c068ca9322b9fe2598e0ae530263083b5fe6
RAntoon <ryan.antoon@gmail.com>
2026-01-03 22:49:03 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@ import dynamic from "next/dynamic";
 
 const MapView = dynamic(() => import("./components/MapView"), { ssr: false });
 
-export default function HomePage() {
-  return (
-    <main style={{ height: "100vh", width: "100vw" }}>
-      <MapView />
-    </main>
-  );
+export default function Page() {
+  return <MapView />;
 }
