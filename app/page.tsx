99abc6da93d356a5a8030bc693ea41f46d5558e5
RAntoon <ryan.antoon@gmail.com>
2026-01-04 00:04:20 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 
-const MapView = dynamic(() => import("./components/MapView"), { ssr: false });
+const MapView = dynamic(() => import("./components/MapView"), {
+  ssr: false,
+});
 
 export default function Page() {
   return <MapView />;
