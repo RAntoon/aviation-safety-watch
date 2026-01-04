8a70f22f27c0de5c4c1777d37a8300bb90244a6f
RAntoon <ryan.antoon@gmail.com>
2026-01-04 00:22:03 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-// app/page.tsx
-import MapView from "./components/MapView";
+import dynamic from "next/dynamic";
+
+const MapView = dynamic(() => import("./components/MapView"), {
+  ssr: false, // Leaflet must run client-side
+});
 
 export default function Page() {
   return <MapView />;
