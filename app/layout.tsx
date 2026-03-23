92916945ff773aac84925b5baa8b51b494d87484
Ryan Antoon <ryan.antoon@gmail.com>
2026-03-23 16:25:17 -0700

    Add Google AdSense code

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google AdSense */}
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3543230523126819"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+        />
+
         {/* Google Analytics 4 */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-CH8DN2MYWB"
